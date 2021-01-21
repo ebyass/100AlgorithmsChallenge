0a3887db63f58389e633313c62850d39e3c21044
Eleanor Byass <eleanorbyass@gmail.com>
2021-01-21 16:39:00 +0000

    higher order function in TypeScript

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -2,7 +2,7 @@ export function absoluteValuesSumMinimization(a: number[]): number {
 		// const isEven = a.length % 2 === 0
 		// return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)]
 
-		const data = a.reduce(function(a, b){
+		const data = a.reduce(function(a:number, b:number){
 			return a + b
 		})
 		let result = data / a.length 
