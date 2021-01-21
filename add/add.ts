037da2929e549432f0f640d669fb2aaaeea45187
Eleanor Byass <eleanorbyass@gmail.com>
2021-01-21 17:01:14 +0000

    repeat string method, concat string method and refactoring

--- add/add.ts
+++ add/add.ts
@@ -9,9 +9,9 @@ function add2(...param1: number[]): number {
 	})
 
 	return total
-}
-console.log(add(1, 2));
-console.log(add(3, 2));
+// }
+// console.log(add(1, 2));
+// console.log(add(3, 2));
 
-console.log(add2(1,2,3,4,5));
-console.log(add2(2,3));
+// console.log(add2(1,2,3,4,5));
+// console.log(add2(2,3));
