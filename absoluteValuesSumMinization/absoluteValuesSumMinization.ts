036b65e35327210557a8a4408fa994be37a3e989
Eleanor Byass <eleanorbyass@gmail.com>
2021-01-21 16:33:32 +0000

    math algorithm

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,17 @@
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+		// const isEven = a.length % 2 === 0
+		// return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)]
+
+		const data = a.reduce(function(a, b){
+			return a + b
+		})
+		let result = data / a.length 
+		return Math.floor(result)
+
+
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
