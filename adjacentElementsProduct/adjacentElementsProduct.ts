7cecd52e2e39ef934dc7d9f9923d7af56cf6215a
Eleanor Byass <eleanorbyass@gmail.com>
2021-01-22 11:46:45 +0000

    longest strings

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,11 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
+	let highestNum = 0
+	for(let i=0; i<inputArray.length-1; i++){
+		let product = inputArray[i] * inputArray[i+1]
 
+		product > highestNum ? highestNum = product : highestNum
+	}
+	return highestNum
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,17 @@
+import { isTemplateMiddle } from "typescript";
+
 export function allLongestStrings(inputArray: string[]): string[] {
 
+	let maxNum = 0
+
+	for(let i=0; i<inputArray.length; i++) {
+		inputArray[i].length > maxNum ? maxNum = inputArray[i].length : maxNum
+	}
+
+	const res = inputArray.filter(item => {
+		return item.length === maxNum
+	})
+	return res
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
